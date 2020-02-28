fe326ceeb8e7c491b9350b2e8778f037bec6d69b
JasonWu <15000257149@163.com>
2020-02-28 20:46:57 +0800

    update code

--- Common.py
+++ Common.py
@@ -14,11 +14,12 @@
 import requests
 
 
+# 定义一个common的类，它的父类是object
 class Common(object):
     # common的构造函数
-    def __init__(self):
-        # 被测系统的根路由
-        self.url_root = 'http://127.0.0.1:12356'
+    def __init__(self,url_root):
+        # 被测系统的跟路由
+        self.url_root = url_root
     # 封装你自己的get请求，uri是访问路由，params是get请求的参数，如果没有默认为空
     def get(self, uri, params=''):
         # 拼凑访问地址
@@ -39,4 +40,4 @@ class Common(object):
             # 如果无参数，访问方式如下
             # 返回request的Response结果，类型为requests的Response类型
             res = requests.post(url)
-        return res
+            return res
